b493ae2d26e0593daea18c7bf18386e6fd51beac
escobarnatalia <nataliaescfer@gmail.com>
2020-03-10 10:16:00 -0500

    cree el archivo y el repositorio

---
+++ Taller_Tesla/src/tesla/Main.java
@@ -0,0 +1,24 @@
+package tesla;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main(Main.class.getName());
+
+	}
+	
+	public void setting () {
+		size(2317,5250);
+	}
+	
+	public void setup() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+
+}
